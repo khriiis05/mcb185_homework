e24d5a8480a838a395cc83b119d4017d9f8dcce3
khriiis05 <klopez1905@gmail.com>
2026-01-22 10:55:06 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -43,12 +43,54 @@ a = 3
 b = 4
 c = math.sqrt(a**2 + b**2)
 print(c)
-
 print(type(a), type (b), type(c), sep='', end='!\n')
-
 
 #Functions
 def pythagoras(a, b):
 	return math.sqrt(a**2 + b**2)
-
 print(pythagoras(3,4))
+
+#Function Practice
+def circle_area(r):
+	return math.pi * r**2
+print(circle_area(4))
+
+def rectangle_area(w, h):
+	return w * h
+print(rectangle_area(6, 7))
+
+def triangle_area(w, h):
+	return rectangle_area(w, h) / 2
+print(triangle_area(6, 7))
+
+def fah_to_cel(fah):
+	return (fah - 32) * 5/9
+print(fah_to_cel(99))
+
+def cel_to_fah(cel):
+	return (cel * 9/5) + 32
+print(cel_to_fah(37))
+
+def mph_to_kph(mph):
+	return mph * 1.609
+print(mph_to_kph(79))
+
+def DNA(OD260):
+	return OD260 * 50
+print(DNA(0.8))
+
+def arith_mean(x, y, z):
+	return (x + y + z) / 3
+print(arith_mean(10, 15, 20))
+
+def geo_mean(x, y, z):
+	return (x * y * z)**(1/3)
+print(geo_mean(3, 6, 12))
+
+def har_mean(x, y, z):
+	return 3 / ((1 / x) + (1 / y) + (1 / z))
+print(har_mean(8, 7, 3))
+
+def distance(x1, y1, x2, y2):
+	return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print(distance(3, 2, 5, 6))
