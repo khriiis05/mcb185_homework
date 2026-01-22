5152c42eb2e949dfe49e5482044f911247e48922
khriiis05 <klopez1905@gmail.com>
2026-01-21 21:44:31 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -21,7 +21,6 @@ print(5 * (2 + 2))
 print(abs(-7))
 print(pow(2,3))
 print(round(0.789, ndigits=2))
-
 print(math.ceil(576.87))
 print(math.floor(576.87))
 print(math.log(4))
@@ -30,10 +29,26 @@ print(math.log10(4))
 print(math.sqrt(25))
 print(math.pow(2, 3))
 print(math.factorial(6))
-
 print(math.e)
 print(math.pi)
 print(math.inf)
 print(math.nan)
 
-#print(1/0)
+# Numbers Aren't Real
+print(0.1 * 1)
+print(0.1 * 3)
+
+#Variables
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print(c)
+
+print(type(a), type (b), type(c), sep='', end='!\n')
+
+
+#Functions
+def pythagoras(a, b):
+	return math.sqrt(a**2 + b**2)
+
+print(pythagoras(3,4))
